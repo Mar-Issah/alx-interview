5bdf2824c82bd12e690b4de7369e174eeafe70c5
Mar-Issah <masy370@gmail.com>
2023-12-14 17:28:15 -0600

    Feat: implement minOperations func

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,22 +4,19 @@
 
 def minOperations(n):
     """
-    minOperations
     Gets fewest # of operations needed to result in exactly n H characters
+    Args:
+    n: The number of times
+    Returns: operations
+
     """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
     if (n < 2):
         return 0
-    ops, root = 0, 2
+    operations, root = 0, 2
     while root <= n:
-        # if n evenly divides by root
         if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
+            operations += root
             n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
             root -= 1
-        # increment root until it evenly-divides n
         root += 1
-    return ops
+    return operations
