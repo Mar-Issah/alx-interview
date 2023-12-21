f7f3cb192f8ad125f1f2e4eb2c76f1fcbee70a3d
Mar-Issah <masy370@gmail.com>
2023-12-20 23:39:01 -0600

    Fix: pycodestyle error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -21,15 +21,8 @@ def _print(status_code, file_size):
             print("{}: {}".format(key, value))
 
 
-
-status_code = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
 code = 0
 counter = 0
